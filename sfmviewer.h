7acede220f426eb6681fbf0f4555648e3e7e12b9
Felix Löhr <fe.loehr@gmail.com>
2013-11-07 15:19:19 +0100

    Moved som methods to cpp file. Added method invokation, so OpenGL context gets only called by main thread.

--- sfmviewer.h
+++ sfmviewer.h
@@ -17,6 +17,10 @@
 
 #include "MultiCameraPnP.h"
 
+Q_DECLARE_METATYPE(std::vector<cv::Point3d>)
+Q_DECLARE_METATYPE(std::vector<cv::Vec3b>)
+Q_DECLARE_METATYPE(std::vector<cv::Matx34d>)
+
 void open_imgs_dir(const char* dir_name, std::vector<cv::Mat>& images, std::vector<std::string>& images_names, double downscale_factor);
 
 class SFMViewer : public QGLViewer, public SfMUpdateListener, public QRunnable
@@ -42,12 +46,8 @@ protected :
 	virtual void init();
 
 public:
-	SFMViewer(QWidget *parent = 0):QGLViewer(QGLFormat::defaultFormat(),parent),vizScale(1.0) {
-		distance = new MultiCameraPnP();
-		distance->attach(this);
-		m_global_transform = Eigen::Affine3d::Identity();
-	}
-    ~SFMViewer() { saveStateToFile(); }
+    SFMViewer(QWidget *parent = 0);
+    ~SFMViewer();
 
     virtual void update(std::vector<cv::Point3d> pcld,
 			std::vector<cv::Vec3b> pcldrgb,
@@ -56,34 +56,17 @@ public:
 			std::vector<cv::Matx34d> cameras);
 
 
-	void run() { distance->RecoverDepthFromImages(); }
+    void run();
 
 public slots:
-	void openDirectory() {
-		images.clear();images_names.clear();
-		std::string imgs_path = QFileDialog::getExistingDirectory(this, tr("Open Images Directory"), ".").toStdString();
-		double scale_factor = 1.0;
-        QLineEdit* l = parentWidget()->findChild<QLineEdit*>("lineEdit_scaleFactor");
-        if(l) {
-        	scale_factor = l->text().toFloat();
-        	std::cout << "downscale to " << scale_factor << std::endl;
-        }
-		open_imgs_dir(imgs_path.c_str(),images,images_names,scale_factor);
-		if(images.size() == 0) {
-			std::cerr << "can't get image files" << std::endl;
-		} else {
-			distance->setImages(images,images_names,imgs_path);
-		}
-	}
-	void setUseRichFeatures(bool b) {distance->use_rich_features = b;}
-	void setUseGPU(bool b) {distance->use_gpu = b;}
-	void runSFM() {
-		this->setAutoDelete(false);
-		m_pcld.clear();
-		m_pcldrgb.clear();
-		m_cameras.clear();
-		m_cameras_transforms.clear();
-		QThreadPool::globalInstance()->start(this);
-	}
-	void setVizScale(int i) { vizScale = (float)(i); updateGL(); }
+    void openDirectory();
+    void setUseRichFeatures(bool b);
+    void setUseGPU(bool b);
+    void runSFM();
+    void setVizScale(int i);
+    void updatePointCloud(std::vector<cv::Point3d> pcld,
+                          std::vector<cv::Vec3b> pcldrgb,
+                          std::vector<cv::Point3d> pcld_alternate,
+                          std::vector<cv::Vec3b> pcldrgb_alternate,
+                          std::vector<cv::Matx34d> cameras);
 };
